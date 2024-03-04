3b870db4189b971a1415e973b91d5d58bd256861
Krishna Mandal <kumandal@student.apc.edu.ph>
2024-03-04 13:22:00 +0800

    contact complete

---
+++ ci4/app/Views/pages/contact.php
@@ -0,0 +1,47 @@
+<div class="content_header"><h2><br /><br /><br /><br />CONTACT ME</h2><h4>If you have anything to ask outside the site, send a message over in these places.</h4></div>
+<div class="contact_div">
+<a href="javascript:void(0)" class="discord_icon">
+<?php
+$contIcon = [
+	'src' => 'images/discord.png',
+	'alt' => 'Discord',
+	'class' => 'contact_icon'
+];
+echo img($contIcon);
+?>
+<div class="discord_text">
+<span class="discord_tri"></span>
+<p>@eurasian</p>
+</div>
+</a>
+<a href="https://m.me/krishna.mandal.812/">
+<?php
+$contIcon = [
+	'src' => 'images/fb.png',
+	'alt' => 'Facebook Messenger',
+	'class' => 'contact_icon'
+];
+echo img($contIcon);
+?>
+</a>
+<a href="mailto:kumandal@student.apc.edu.ph">
+<?php
+$contIcon = [
+	'src' => 'images/email1.png',
+	'alt' => 'APC Email',
+	'class' => 'contact_icon'
+];
+echo img($contIcon);
+?>
+</a>
+<a href="mailto:krinda876@gmail.com">
+<?php
+$contIcon = [
+	'src' => 'images/email2.png',
+	'alt' => 'Personal Email',
+	'class' => 'contact_icon'
+];
+echo img($contIcon);
+?>
+</a>
+</div>
