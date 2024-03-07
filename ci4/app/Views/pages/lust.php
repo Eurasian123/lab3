71646c1f169a07a319654a867c4501d37fec35ed
Krishna Mandal <kumandal@student.apc.edu.ph>
2024-03-08 07:20:39 +0800

    lust complete

--- ci4/app/Views/pages/lust.php
+++ ci4/app/Views/pages/lust.php
@@ -6,40 +6,60 @@
 <div class="lust_container">
 <div class="lust_item" id="ultrakill">
 <div class="image_float">
+<div class="limage_cont">
 <?php echo img('images/lust/ultrakill.jpg'); ?>
+<video muted><source src="images/lust/uk.mp4" type="video/mp4"></video>
+</div>
 </div>
 <h1>ULTRAKILL</h1>
-<p>ULTRAKILL is a fast-paced shooter RPG Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar purus at est facilisis tristique. Fusce vitae nibh vitae purus gravida rhoncus a non quam. Cras felis ipsum, viverra in erat et, imperdiet finibus justo. Sed efficitur, ligula eu pulvinar imperdiet, metus risus aliquam nibh, ac consequat urna nisl id augue.</p>
+<p>ULTRAKILL is a fast-paced first-person shooter. Based on a post-extinction setting, the game follows a robot named V1 as it descends Hell for blood.</p>
 <p>As you can see, I'm obsessed with this game, and for good reason. The gameplay is fun, the writing is well-done, the lore is intriguing, and the music hits. I would like to say more, but my mind can utter little. It's simply too great for me to explain in just a paragraph. Just go play it.</p>
 </div>
 <div class="lust_item" id="lethalcom">
 <div class="image_float">
+<div class="limage_cont">
 <?php echo img('images/lust/lethal_company.jpg'); ?>
+<video muted><source src="images/lust/lc.mp4" type="video/mp4"></video>
+</div>
 </div>
 <h1>Lethal Company</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar purus at est facilisis tristique. Fusce vitae nibh vitae purus gravida rhoncus a non quam. Cras felis ipsum, viverra in erat et, imperdiet finibus justo. Sed efficitur, ligula eu pulvinar imperdiet, metus risus aliquam nibh, ac consequat urna nisl id augue. Duis consectetur iaculis orci, auctor dapibus nunc imperdiet ultricies. Vivamus dapibus lobortis varius. Nulla mi elit, cursus sit amet lacus sit amet, ornare consectetur odio. Praesent orci felis, commodo eget feugiat quis, egestas ut elit. Donec convallis metus congue urna faucibus porta varius tempor tortor. Nam ornare, libero id sodales sollicitudin, magna velit condimentum elit, at aliquet magna ante feugiat elit. Praesent auctor nec augue nec mollis. In dapibus velit at dolor mattis ullamcorper.</p>
+<p>Lethal Company is a first-person co-op horror game. Set in a distant retro future, the game is about a team of contracted employees working for "The Company" to collect scrap on abandoned moons.</p>
+<p>I didn't expect to fall in love with this game the way I did. While it is amazing to play with others, I've played this most often solo&mdash; usually an incredibly difficult task. I personally am not a fan of horror at all, yet Lethal Company scratches that itch and makes me brave into the unknown simply for the serotonin of collecting leftover scrap. The game is very well-designed despite being in early access, and is plenty fun to play, especially once a week. This is a game that combines diegetic comedy and incredibly effective horror all into one experience, looped over and over with each moon yet always serving a new experience.</p>
 </div>
 <div class="lust_item" id="hoyov">
 <div class="lust_hoyodiv" id="genshin">
 <div class="image_float">
+<div class="limage_cont">
 <?php echo img('images/lust/genshin_impact.jpg'); ?>
+<video muted><source src="images/lust/gi.mp4" type="video/mp4"></video>
+</div>
 </div>
 <h1>Genshin Impact</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar purus at est facilisis tristique. Fusce vitae nibh vitae purus gravida rhoncus a non quam. Cras felis ipsum, viverra in erat et, imperdiet finibus justo. Sed efficitur, ligula eu pulvinar imperdiet, metus risus aliquam nibh, ac consequat urna nisl id augue. Duis consectetur iaculis orci, auctor dapibus nunc imperdiet ultricies. Vivamus dapibus lobortis varius. Nulla mi elit, cursus sit amet lacus sit amet, ornare consectetur odio. Praesent orci felis, commodo eget feugiat quis, egestas ut elit. Donec convallis metus congue urna faucibus porta varius tempor tortor. Nam ornare, libero id sodales sollicitudin, magna velit condimentum elit, at aliquet magna ante feugiat elit. Praesent auctor nec augue nec mollis. In dapibus velit at dolor mattis ullamcorper.</p>
+<p>Genshin Impact is a high fantasy action RPG that takes place in the diverse world of Teyvat. The game follows the Traveler, a lost outlander who is searching for their sibling.</p>
+
+<p>If it were up to playtime alone, many would assume that Genshin would be my favorite game at this point. While it's not wrong, it's not exactly right either.<br />Besides the point, Genshin is a great game, with responsive gameplay, a great story (not counting the first three regions), and a breathtaking world. The world design and lore is the game's strongest suit, and also my biggest interest. Just the vast array of places and biomes even respective to each region; every symbolism and inspiration thoroughly researched to make a more grandiose world. Most importantly, the music is always incredible.</p>
 </div>
 <div class="lust_hoyodiv" id="hstarl">
 <div class="image_float">
+<div class="limage_cont">
 <?php echo img('images/lust/honkai_sr.jpg'); ?>
+<video muted><source src="images/lust/sr.mp4" type="video/mp4"></video>
+</div>
 </div>
 <h1>Honkai: Star Rail</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar purus at est facilisis tristique. Fusce vitae nibh vitae purus gravida rhoncus a non quam. Cras felis ipsum, viverra in erat et, imperdiet finibus justo. Sed efficitur, ligula eu pulvinar imperdiet, metus risus aliquam nibh, ac consequat urna nisl id augue. Duis consectetur iaculis orci, auctor dapibus nunc imperdiet ultricies. Vivamus dapibus lobortis varius. Nulla mi elit, cursus sit amet lacus sit amet, ornare consectetur odio. Praesent orci felis, commodo eget feugiat quis, egestas ut elit. Donec convallis metus congue urna faucibus porta varius tempor tortor. Nam ornare, libero id sodales sollicitudin, magna velit condimentum elit, at aliquet magna ante feugiat elit. Praesent auctor nec augue nec mollis. In dapibus velit at dolor mattis ullamcorper.</p>
+<p>Honkai: Star Rail is a turn-based RPG, which takes place in a universe of high stellar technological advancement, where ideas are so powerful that there exists "Aeons" who are the embodiments of certain ideologies, known as "Paths." The game follows the Astral Express, who follow the Path of the Trailblaze.</p>
+<p>Another surprise! While I initially disliked it due to its popularity in betas, trying for the first time, I was hooked. But also, many others were too. It didn't matter that it was a turn-based RPG, it still got massively popular because it used Genshin's formula for success. Due to this, it has very much the same qualities as Genshin with even less cons.</p>
 </div>
 <div class="lust_hoyodiv" id="hithird">
 <div class="image_float">
+<div class="limage_cont">
 <?php echo img('images/lust/honkai_impact.png'); ?>
+<video muted><source src="images/lust/hi.mp4" type="video/mp4"></video>
+</div>
 </div>
 <h1>Honkai Impact 3rd</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar purus at est facilisis tristique. Fusce vitae nibh vitae purus gravida rhoncus a non quam. Cras felis ipsum, viverra in erat et, imperdiet finibus justo. Sed efficitur, ligula eu pulvinar imperdiet, metus risus aliquam nibh, ac consequat urna nisl id augue. Duis consectetur iaculis orci, auctor dapibus nunc imperdiet ultricies. Vivamus dapibus lobortis varius. Nulla mi elit, cursus sit amet lacus sit amet, ornare consectetur odio. Praesent orci felis, commodo eget feugiat quis, egestas ut elit. Donec convallis metus congue urna faucibus porta varius tempor tortor. Nam ornare, libero id sodales sollicitudin, magna velit condimentum elit, at aliquet magna ante feugiat elit. Praesent auctor nec augue nec mollis. In dapibus velit at dolor mattis ullamcorper.</p>
+<p>Honkai Impact 3rd is a 3D action RPG, but also features inspirations from similar genres like hack-and-slash. The game takes place in a significantly more technologically advanced modern world, where the Earth is facing another apocalypse as it does every 50000 years. Part 1 focuses on a trio of in-training Valkyries, Kiana, Mei, and Bronya. Part 2, taking place on Mars, focuses on a new cast of characters, particularly Coralie, Helia, Dreamseeker, and Senadina.</p>
+<p>Who doesn't love a good action rpg? While I can't comment much on the game, I can always say the writing <i>hits</i> when the writing hits.</p>
 </div>
 <div class="hoyo_button" onclick="plusConts(1)">
 <?php echo img('images/reload.png'); ?>
@@ -47,17 +67,26 @@
 </div>
 <div class="lust_item" id="gdash">
 <div class="image_float">
+<div class="limage_cont">
 <?php echo img('images/lust/geometry_dash.jpg'); ?>
+<video muted><source src="images/lust/gd.mp4" type="video/mp4"></video>
+</div>
 </div>
 <h1>Geometry Dash</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar purus at est facilisis tristique. Fusce vitae nibh vitae purus gravida rhoncus a non quam. Cras felis ipsum, viverra in erat et, imperdiet finibus justo. Sed efficitur, ligula eu pulvinar imperdiet, metus risus aliquam nibh, ac consequat urna nisl id augue. Duis consectetur iaculis orci, auctor dapibus nunc imperdiet ultricies. Vivamus dapibus lobortis varius. Nulla mi elit, cursus sit amet lacus sit amet, ornare consectetur odio. Praesent orci felis, commodo eget feugiat quis, egestas ut elit. Donec convallis metus congue urna faucibus porta varius tempor tortor. Nam ornare, libero id sodales sollicitudin, magna velit condimentum elit, at aliquet magna ante feugiat elit. Praesent auctor nec augue nec mollis. In dapibus velit at dolor mattis ullamcorper.</p>
+<p>Geometry Dash is a side-scrolling music platforming game series. You have a cube, and you jump. What's else to say? It's a 2012 game.</p>
+<p>Honestly, I don't feel much about the game itself nor its community. The reason it's on this list is because with the release of 2.2, I've come and picked it up again. While I'm still not an avid player, I've definitely improved in some regard. I also have two ongoing level projects, neither of which will likely see the light of day because college and all. Why do I even bother? Creation, I guess.</p>
 </div>
 <div class="lust_item" id="soulsring">
 <div class="image_float">
+<div class="limage_cont">
 <?php echo img('images/lust/elden_ring.jpg'); ?>
+<video muted><source src="images/lust/er.mp4" type="video/mp4"></video>
+</div>
 </div>
 <h1>Soulsborne Ring</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar purus at est facilisis tristique. Fusce vitae nibh vitae purus gravida rhoncus a non quam. Cras felis ipsum, viverra in erat et, imperdiet finibus justo. Sed efficitur, ligula eu pulvinar imperdiet, metus risus aliquam nibh, ac consequat urna nisl id augue. Duis consectetur iaculis orci, auctor dapibus nunc imperdiet ultricies. Vivamus dapibus lobortis varius. Nulla mi elit, cursus sit amet lacus sit amet, ornare consectetur odio. Praesent orci felis, commodo eget feugiat quis, egestas ut elit. Donec convallis metus congue urna faucibus porta varius tempor tortor. Nam ornare, libero id sodales sollicitudin, magna velit condimentum elit, at aliquet magna ante feugiat elit. Praesent auctor nec augue nec mollis. In dapibus velit at dolor mattis ullamcorper.</p>
+<p>The only entry on this list that isn't of a single theme. The Soulsborne Ring games are all RPGs with an emphasis on difficulty. For this page, "Soulsborne Ring" encompasses Dark Souls, Dark Souls III, Bloodborne, and Elden Ring.</p>
+<p>These games are an absolute treat both to watch and play. While lauded as complicated, the comprehension for Soulsborne Ring lore is quite simple and straightforward. Regardless, the writers know how to cook.</p>
+<p><i>The use of Elden Ring here as the game is solely for logistical reasons.</i></p>
 </div>
 
 <div class="lust_prev" onclick="plusSlides(-1)"></div>
--- ci4/public/js/lust.js
+++ ci4/public/js/lust.js
@@ -1,11 +1,61 @@
+let videoList = document.querySelectorAll(".limage_cont video");
+let video;
 let slideIndex = 1;
 let hoyoIndex = 1;
 showSlides(slideIndex);
 showCont(hoyoIndex);
 
+function video_play() {
+	video.load();
+	video.play();
+	video.style.opacity = 1;
+}
+
+function video_pause() {
+	video.pause();
+	video.style.opacity = 0;
+}
+
+function video_end() {
+	video.style.opacity = 0;
+}
+
+function videoVerify() {
+	switch(slideIndex) {
+		case 1:
+			video = videoList[0];
+			break;
+		case 2:
+			video = videoList[1];
+			break;
+		case 3:
+			switch(hoyoIndex) {
+				case 1:
+					video = videoList[2];
+					break;
+				case 2:
+					video = videoList[3];
+					break;
+				case 3:
+					video = videoList[4];
+					break;
+			};
+			break;
+		case 4:
+			video = videoList[5];
+			break;
+		case 5:
+			video = videoList[6];
+			break;
+	};
+}
+
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	video.removeEventListener("mouseenter", video_play);
+	video.removeEventListener("mouseleave", video_pause);
+	video.removeEventListener("ended", video_end);
+	showSlides(slideIndex += n);
 }
 
 function showSlides(n) {
@@ -17,13 +67,20 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
+  videoVerify();
+  video.addEventListener("mouseenter", video_play);
+  video.addEventListener("mouseleave", video_pause);
+  video.addEventListener("ended", video_end);
 }
 
 // HOYO TIME
 
 // Next/previous controls
 function plusConts(n) {
-  showCont(hoyoIndex += n);
+	video.removeEventListener("mouseenter", video_play);
+	video.removeEventListener("mouseleave", video_pause);
+	video.removeEventListener("ended", video_end);
+	showCont(hoyoIndex += n);
 }
 
 function showCont(n) {
@@ -35,4 +92,8 @@ function showCont(n) {
     hoyoconts[i].style.display = "none";
   }
   hoyoconts[hoyoIndex-1].style.display = "block";
+  videoVerify();
+  video.addEventListener("mouseenter", video_play);
+  video.addEventListener("mouseleave", video_pause);
+  video.addEventListener("ended", video_end);
 }
